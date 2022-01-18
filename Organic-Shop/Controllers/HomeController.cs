758ce85c10f2be1b809455a271e7a393801d4896
HoangDung3009 <hoangdung123asd@gmail.com>
2022-01-18 09:18:48 +0700

    add product GUI

--- Organic-Shop/Controllers/HomeController.cs
+++ Organic-Shop/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Organic_Shop.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Organic-Shop/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Organic_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organic_Shop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly OrganicShopContext _context;
+
+        public ProductsController(OrganicShopContext _context)
+        {
+            this._context = _context; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.Include(x => x.Cat).FirstOrDefaultAsync(x => x.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+    }
+}
