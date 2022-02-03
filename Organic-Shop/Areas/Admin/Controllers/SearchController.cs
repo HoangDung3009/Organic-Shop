aeb24e744a544ae821d1f0a4c5c0c53a29416388
HoangDung3009 <hoangdung123asd@gmail.com>
2022-02-03 10:01:37 +0700

    Add Search Product feature

---
+++ Organic-Shop/Areas/Admin/Controllers/SearchController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Organic_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organic_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SearchController : Controller
+    {
+
+        private readonly OrganicShopContext _context;
+
+        public SearchController(OrganicShopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult SearchProduct(string keyword)
+        {
+            List<Product> ls = new List<Product>();
+
+            if(string.IsNullOrEmpty(keyword) || keyword.Length < 1)
+            {
+                return PartialView("SearchProductPartial", null);
+            }
+
+            ls = _context.Products
+                                        .AsNoTracking()
+                                        .Include(x => x.Cat)
+                                        .Where(x => x.ProductName.Contains(keyword))
+                                        .OrderByDescending(x => x.ProductId)
+                                        .Take(10)
+                                        .ToList();
+
+            if(ls == null)
+            {
+                return PartialView("SearchProductPartial", null);
+            }
+            else
+            {
+                return PartialView("SearchProductPartial", ls);
+            }
+        }
+    }
+}
