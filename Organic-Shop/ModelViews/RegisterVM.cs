9ee7cbfe9100c3ee107becc3ed7a397ee1eff1c7
HoangDung3009 <hoangdung123asd@gmail.com>
2022-02-27 00:31:09 +0700

    add model views

---
+++ Organic-Shop/ModelViews/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organic_Shop.ModelViews
+{
+    public class RegisterVM
+    {
+        [Key]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Please enter email !!")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter password !!")]
+        //  [MinLength(5, ErrorMessage = "Min length is 5")]
+        public string Password { get; set; }
+    }
+}
